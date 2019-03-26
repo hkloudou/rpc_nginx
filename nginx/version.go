e0f724d7a5be58e5794f13f901427742f8438960
hkloudou <hkloudou@gmail.com>
2019-03-26 19:56:12 +0800

    auto commit

--- nginx/version.go
+++ nginx/version.go
@@ -1,6 +1,6 @@
 package nginx
 
 //Version git tag version
-var Version = "v1.0.16"
-var VersionNilDot = "v1-0-16"
-var GRPCServerURL = "v1-0-16-rpc-nginx.grpc.apiatm.com"
+var Version = "v1.0.18"
+var VersionNilDot = "v1-0-18"
+var GRPCServerURL = "rpc-nginx-v1-0-18.grpc.apiatm.com"
